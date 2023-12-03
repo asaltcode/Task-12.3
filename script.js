b764bb494fdb3f5d7d91f4086408845edfda70c8
asaltcode <elangovanr558@gmail.com>
2023-12-03 11:12:32 +0530

    extra future added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const cityName = document.getElementById("cityName");
 const getBtn =document.getElementById("getBtn");
-const currentWeather = document.querySelector(".currentWeather")
-const min_temp = document.querySelector(".min-temp")
+const currentWeather = document.querySelector(".currentWeather");
+const min_temp = document.querySelector(".min-temp");
 const max_temp = document.querySelector(".max-temp");
-const weather_icon = document.getElementById("weather_icon")
+const city_name = document.querySelector(".city-name")
+const weather_icon = document.getElementById("weather_icon");
 
 //getting weaather from tamlnadu
 function getWeather(city){
@@ -23,7 +24,8 @@ function cityWeather(lat, lon){
     const response = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c2992e8e9788c5d112051a7a743a1b70&units=metric`);
     response.then((datas)=>datas.json())
     .then((data)=>{ //assion the value
-        currentWeather.lastElementChild.innerHTML = Math.round(data.main.feels_like);
+        console.log(data);
+        currentWeather.lastElementChild.innerHTML = Math.floor(data.main.temp);
         min_temp.lastElementChild.innerHTML = Math.floor(data.main.temp_min);
         max_temp.lastElementChild.innerHTML = Math.floor(data.main.temp_max);
         weather_icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
@@ -36,10 +38,19 @@ function cityWeather(lat, lon){
 getBtn.addEventListener("click", ()=>{
     //input validation
     let value = cityName.value
-    if(isNaN(value)){        
+    if(isNaN(value)){     
+        city_name.lastElementChild.innerHTML = value   
         getWeather(cityName.value);
+        cityName.value = ""
     }
     else{
         alert("Enter the valid input")
     }
+});
+//button click effect
+getBtn.addEventListener("mousedown", ()=>{
+    getBtn.style.transform = "scale(0.9)"
+    this.addEventListener("mouseup", ()=>{
+        getBtn.style.transform = "scale(1)"
+    })
 })
